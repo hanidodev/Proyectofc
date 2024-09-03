b495d199e66f8261f1ff6a1edbc89d2f5d80d659
hanidodev <hanidodev@gmail.com>
2024-09-03 19:02:43 +0200

    Nuevo proyecto android studio

---
+++ ProyectoFC/src/proyectofc/ProyectoFC.java
@@ -0,0 +1,36 @@
+package proyectofc;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hecto
+ */
+public class ProyectoFC extends Application {
+    
+    /**
+     *
+     * @param stage
+     * @throws java.io.IOException
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        throw new UnsupportedOperationException("No soportado");
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+    }
+    
+}
