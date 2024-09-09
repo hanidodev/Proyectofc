5aab22d7268f5a0627f2b894398053d066b0712a
hanidodev <hanidodev@gmail.com>
2024-09-09 17:59:20 +0200

    navigation addition

--- ProyectoFCAS/app/build.gradle.kts
+++ ProyectoFCAS/app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -45,4 +46,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
