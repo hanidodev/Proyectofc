381e81ff7bce4a5356af101c2cddc6adf4339ffe
hanidodev <hanidodev@gmail.com>
2024-10-11 23:43:01 +0200

    desde AStudio

--- Proyectofc/ProyectoFCAS/app/src/main/java/com/example/proyectofcas/PrincipalFragment.kt
+++ Proyectofc/ProyectoFCAS/app/src/main/java/com/example/proyectofcas/PrincipalFragment.kt
@@ -16,6 +16,7 @@ import model.AppDatabase
 import model.GastoDao
 
 class PrincipalFragment : Fragment() {
+    lateinit var pruebas:String
 
     private var _bindingPrincipal:FragmentPrincipalBinding? = null
     private val bindingPrincipal:FragmentPrincipalBinding
